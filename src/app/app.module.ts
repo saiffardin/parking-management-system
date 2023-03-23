fb567a11a4a43f90f06ef95b8ae2b4613c8c160f
saiffardin <saiffardin@gmail.com>
2023-03-23 12:21:58 +0600

    navbar component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    DashboardComponent
+    DashboardComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
